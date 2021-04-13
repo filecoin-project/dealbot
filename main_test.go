a0b822933250d86768a4993f89561423e39f60ad
Anton Evangelatov <anton.evangelatov@gmail.com>
2021-04-13 16:32:44 +0300

    fixup

--- main_test.go
+++ main_test.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"os"
 	"testing"
 
+	"github.com/filecoin-project/dealbot/devnet"
 	"github.com/rogpeppe/go-internal/testscript"
 )
 
-func TestFoo(t *testing.T) {
+func TestMain(m *testing.M) {
+	os.Exit(testscript.RunMain(m, map[string]func() int{
+		"devnet": func() int {
+			devnet.Main()
+			return 0
+		},
+	}))
+}
+
+func TestStorageAndRetrievalTasks(t *testing.T) {
 	testscript.Run(t, testscript.Params{
 		Dir: "testdata/scripts",
 	})
