1acf3852f96d0ec5412eff7ef17ef95b9a8be4ad
Anton Evangelatov <anton.evangelatov@gmail.com>
2021-04-14 20:31:42 +0300

    run only one storage deal

--- integration_tests/01_storage_retrieval_ok.sh
+++ integration_tests/01_storage_retrieval_ok.sh
@@ -15,15 +15,7 @@ dealbot --api $TOKEN:$API storage-deal --data-dir $DATADIR --miner $MINER
 
 returnValue=$?
 if [[ $returnValue -ne 0 ]]; then
-    echo "expected first storage-deal to succeed, but it returned exit code != 0"
-    exit 1
-fi
-
-dealbot --api $TOKEN:$API storage-deal --data-dir $DATADIR --miner $MINER
-
-returnValue=$?
-if [[ $returnValue -ne 0 ]]; then
-    echo "expected second storage-deal to succeed, but it returned exit code != 0"
+    echo "expected storage-deal to succeed, but it returned exit code != 0"
     exit 1
 fi
 
