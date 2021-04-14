138094813ff5e0c131a1d5822739a5dc4e1e6a9c
Anton Evangelatov <anton.evangelatov@gmail.com>
2021-04-14 20:21:03 +0300

    update integration test to pass for both storage deals

--- integration_tests/01_storage_retrieval_ok_local.sh
+++ integration_tests/01_storage_retrieval_ok_local.sh
@@ -14,8 +14,8 @@ MINER=t01000
 dealbot --api $TOKEN:$API storage-deal --data-dir $DATADIR --miner $MINER
 
 returnValue=$?
-if [[ $returnValue -ne 0 ]]; then
-    echo "expected first storage-deal to succeed, but it returned exit code != 0"
+if [[ $returnValue -eq 0 ]]; then
+    echo "expected first storage-deal to fail, but it returned exit code 0"
     exit 1
 fi
 
