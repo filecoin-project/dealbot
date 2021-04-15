2ab0190e303ccc3a765b53d0fa630a8b5f0ae47e
Daniel Martí <mvdan@mvdan.cc>
2021-04-15 17:12:19 +0100

    allow running integration tests against an existing lotus node
    The added tunnel.sh script opens an ssh proxy-forwarding tunnel for the
    API, and sets up the LOTUS_API and LOTUS_TOKEN env vars for dealbot to
    be able to talk to it.
    
    The header.sh script is updated to only run a local devnet if those env
    vars are unset.
    
    Finally, remove 01_storage_retrieval_ok_local, as we no longer seem to
    have issues with the first deail failing.

--- integration_tests/header.sh
+++ integration_tests/header.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 
+my_dir="$(dirname "$0")"
+
 #set -o errexit ... re-enable when we fix the storage deal bug on Lotus side
 set -x
 
-err_report() {
-    echo "Error on line $1 : $2"
-}
-#FILENAME=`basename $0`
-#trap 'err_report $LINENO $FILENAME' ERR
-
-function finish {
-  kill -9 $DEVNETPID
-}
-trap finish EXIT
-
-# go install dealbot and devnet
-my_dir="$(dirname "$0")"
-pushd $my_dir/../
-go install ./...
-popd
-
-TEMPDIR=`mktemp -d`
+export TEMPDIR=$(mktemp -d)
 export HOME=$TEMPDIR
-export LOTUS_PATH=$TEMPDIR/.lotus
-export LOTUS_MINER_PATH=$TEMPDIR/.lotusminer
+
+# If the user specified an existing lotus node, don't start devnet.
+# We return instead of exiting, since this is a source-d script.
+if [[ -z $LOTUS_API ]]; then
+	function finish {
+		kill -9 $DEVNETPID
+	}
+	trap finish EXIT
+
+	# go install dealbot and devnet
+	pushd $my_dir/../
+	go install ./...
+	popd
+	LOTUS_PATH=$TEMPDIR/.lotus
+	LOTUS_MINER_PATH=$TEMPDIR/.lotusminer
+
+	devnet &
+	DEVNETPID=$!
+
+	sleep 60
+
+	export LOTUS_TOKEN=$(cat $LOTUS_PATH/token)
+	export LOTUS_API=$(cat $LOTUS_PATH/api)
+fi
 
 pushd $TEMPDIR
-
-devnet &
-DEVNETPID=$!
-
-sleep 60
