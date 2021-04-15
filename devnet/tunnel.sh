19adef69ab56a8abdc51a194f7184dc5e46acdb9
Daniel Martí <mvdan@mvdan.cc>
2021-04-15 17:12:19 +0100

    allow running integration tests against an existing lotus node
    The added tunnel.sh script opens an ssh proxy-forwarding tunnel for the
    API, and sets up the LOTUS_API and LOTUS_TOKEN env vars for dealbot to
    be able to talk to it.
    
    The header.sh script is updated to only run a local devnet if those env
    vars are unset.
    
    Finally, remove 01_storage_retrieval_ok_local, as we no longer seem to
    have issues with the first deail failing.

---
+++ devnet/tunnel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ADDRESS=ubuntu@ec2-3-83-188-192.compute-1.amazonaws.com
+
+# Start the tunnel to port forward the lotus API on :1234.
+# This is a no-op if the port is already running.
+echo "Connecting tunnel..."
+ssh -N -f -L 1234:localhost:1234 $ADDRESS
+
+# Obtain the api multiaddress and token.
+# Note that we need sudo to read the token.
+# Also note that $LOTUS_PATH is only set for the non-root user.
+# We use awk instead of cat since the files have no trailing newlines.
+echo "Collecting lotus api address and token..."
+VARS=($(ssh -T $ADDRESS 'sudo awk 1 $LOTUS_PATH/api $LOTUS_PATH/token'))
+export LOTUS_API=${VARS[0]}
+export LOTUS_TOKEN=${VARS[1]}
--- integration_tests/01_storage_retrieval_ok.sh
+++ integration_tests/01_storage_retrieval_ok.sh
@@ -3,28 +3,23 @@
 my_dir="$(dirname "$0")"
 source "$my_dir/header.sh"
 
-export LOTUS_PATH=$TEMPDIR/.lotus
-export LOTUS_MINER_PATH=$TEMPDIR/.lotusminer
-
-TOKEN=`cat $LOTUS_PATH/token`
-API=`cat $LOTUS_PATH/api`
 DATADIR=$TEMPDIR
 MINER=t01000
 
-dealbot --api $TOKEN:$API storage-deal --data-dir $DATADIR --miner $MINER
+dealbot --api=$LOTUS_TOKEN:$LOTUS_API storage-deal --data-dir=$DATADIR --miner=$MINER
 
 returnValue=$?
 if [[ $returnValue -ne 0 ]]; then
-    echo "expected storage-deal to succeed, but it returned exit code != 0"
-    exit 1
+	echo "expected storage-deal to succeed, but it returned exit code != 0"
+	exit 1
 fi
 
-CID=`lotus client local | tail -1 | awk '{print $2}'`
+CID=$(lotus client local | tail -1 | awk '{print $2}')
 
-dealbot --api $TOKEN:$API retrieval-deal --data-dir $DATADIR --miner $MINER --cid=$CID
+dealbot --api=$LOTUS_TOKEN:$LOTUS_API retrieval-deal --data-dir=$DATADIR --miner=$MINER --cid=$CID
 
 returnValue=$?
 if [[ $returnValue -ne 0 ]]; then
-    echo "expected retrieval-deal to succeed, but it returned exit code != 0"
-    exit 1
+	echo "expected retrieval-deal to succeed, but it returned exit code != 0"
+	exit 1
 fi
