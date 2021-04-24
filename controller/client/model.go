a8bed35e3f2ee4571d50834433b6c1f8d163f4c7
gammazero <gammazero@users.noreply.github.com>
2021-04-24 13:22:04 -0700

    Not using PopTaskRequest
    Choosing to use UpdateTaskRequest so that clients can set the initial task state. When a task in assigned, it it currently set to InProgress.  At some point in the future there may be different states when a task is assigned to a worker (Scheduled, WaitingApproval, etc.)

--- controller/client/model.go
+++ controller/client/model.go
@@ -6,7 +6,3 @@ type UpdateTaskRequest struct {
 	Status   tasks.Status `json:"status"`
 	WorkedBy string       `json:"worked_by,omitempty"` // which dealbot works on that task
 }
-
-type PopTaskRequest struct {
-	WorkedBy string
-}
