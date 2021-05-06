ce70c6d21f70f89888b9cc7e1cc88a3dc0918bfd
Daniel Martí <mvdan@mvdan.cc>
2021-05-06 15:20:44 +0100

    integration_tests: add first test including the controller
    This test starts a controller and a daemon, adds a storage task to the
    controller queue, and waits for the daemon to pick it up and complete
    it.
    
    	$ bash integration_tests/02_controller_daemon.sh
    	[about five minutes later]
    	daemon.log: [...] engine/engine.go:111 successfully stored data
    	Storage deal is active!
    
    This test is slow, but also pretty barebones; a number of TODOs are
    included for ways in which we could extend it.
    
    We remove "set -x" from the header script, because it would get far too
    noisy with the "grep" querying of the logs until completion.
    
    This commit also includes an improved tunnel script, to work with the new AWS
    machine, plus docs on how to use it.

--- devnet/tunnel.sh
+++ devnet/tunnel.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-ADDRESS=ubuntu@ec2-3-83-188-192.compute-1.amazonaws.com
+# AWS machine running a nerpanet node.
+# Note that deals will be as slow as mainnet.
+# ADDRESS=ubuntu@ec2-3-83-188-192.compute-1.amazonaws.com
+# LOTUS_PATH=/var/lib/lotus # the system default
+
+# AWS machine running devnet.
+ADDRESS=ubuntu@ec2-3-237-19-14.compute-1.amazonaws.com
+LOTUS_PATH=/tmp/devnet-lotus # as per README.md
 
 # Start the tunnel to port forward the lotus API on :1234.
 # This is a no-op if the port is already running.
 echo "Connecting tunnel..."
 ssh -N -f -L 1234:localhost:1234 $ADDRESS || exit 1
 
-# We assume the REMOTE_SSHFS directory exists on the server.
-# If it doesn't, create it and re-run.
+# Make sure the REMOTE_SSHFS directory exists on the server.
 echo "Mounting sshfs temp dir..."
 REMOTE_SSHFS=/tmp/dealbot-sshfs
+ssh -T $ADDRESS "mkdir -p $REMOTE_SSHFS"
+
+# Create the local directory, and mount.
 LOCAL_SSHFS=$(mktemp -d)
 sshfs $ADDRESS:$REMOTE_SSHFS $LOCAL_SSHFS || exit 1
 export DEALBOT_NODE_DATA_DIRECTORY=$REMOTE_SSHFS
@@ -18,9 +27,8 @@ export DEALBOT_DATA_DIRECTORY=$LOCAL_SSHFS
 
 # Obtain the api multiaddress and token.
 # Note that we need sudo to read the token.
-# Also note that $LOTUS_PATH is only set for the non-root user.
 # We use awk instead of cat since the files have no trailing newlines.
 echo "Collecting lotus api address and token..."
-API=$(ssh -T $ADDRESS 'sudo awk 1 $LOTUS_PATH/api')
-TOKEN=$(ssh -T $ADDRESS 'sudo awk 1 $LOTUS_PATH/token')
+API=$(ssh -T $ADDRESS "sudo cat $LOTUS_PATH/api")
+TOKEN=$(ssh -T $ADDRESS "sudo cat $LOTUS_PATH/token")
 export FULLNODE_API_INFO="${TOKEN}:${API}"
