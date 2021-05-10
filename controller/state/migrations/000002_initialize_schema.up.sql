6c70285a35abe6996cc57c92c4364f1d0cb8948e
Andrew Gillis <gammazero@users.noreply.github.com>
2021-05-10 16:31:10 -0700

    Update 000002_initialize_schema.up.sql
    Postgres does not have `string` type.

--- controller/state/migrations/000002_initialize_schema.up.sql
+++ controller/state/migrations/000002_initialize_schema.up.sql
@@ -5,7 +5,7 @@ ALTER TABLE tasks ADD COLUMN cid string;
 CREATE INDEX ix_tasks_cid ON tasks (cid);
 
 CREATE TABLE finalizedData (
-    cid string NOT NULL,
+    cid varchar(255) NOT NULL,
     data text NOT NULL CHECK (data != ''),
     created timestamp NOT NULL,
     PRIMARY KEY(cid)
