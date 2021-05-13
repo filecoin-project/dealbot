1832a80591bff3424893913dc1d4d680eff99d07
Will Scott <will.scott@protocol.ai>
2021-05-13 16:24:18 -0700

    simplify printing

--- metrics/log/log.go
+++ metrics/log/log.go
@@ -8,7 +8,6 @@ import (
 	"github.com/filecoin-project/dealbot/metrics"
 	"github.com/filecoin-project/dealbot/tasks"
 	logging "github.com/ipfs/go-log/v2"
-	"github.com/ipld/go-ipld-prime"
 )
 
 type logRecorder struct {
@@ -24,50 +23,31 @@ func (lr *logRecorder) Handler() http.Handler {
 	return nil
 }
 
-func simpleGoValue(node ipld.Node) interface{} {
-	var val interface{}
-	var err error
-	switch kind := node.Kind(); kind {
-	case ipld.Kind_String:
-		val, err = node.AsString()
-	case ipld.Kind_Int:
-		val, err = node.AsInt()
-	case ipld.Kind_Bool:
-		val, err = node.AsBool()
-	default:
-		panic(kind.String())
-	}
-	if err != nil {
-		panic(err)
-	}
-	return val
-}
-
 func (lr *logRecorder) ObserveTask(task tasks.Task) error {
 	duration := time.Since(task.StartedAt.Must().Time()).Milliseconds()
 
 	if task.RetrievalTask.Exists() {
 		rt := task.RetrievalTask.Must()
 		lr.log.Infow("retrieval task",
-			metrics.UUID, simpleGoValue(&task.UUID),
-			metrics.Miner, simpleGoValue(&rt.Miner),
-			metrics.PayloadCID, simpleGoValue(&rt.PayloadCID),
-			metrics.CARExport, simpleGoValue(&rt.CARExport),
-			metrics.Status, simpleGoValue(&task.Status),
+			metrics.UUID, task.UUID.String(),
+			metrics.Miner, rt.Miner.String(),
+			metrics.PayloadCID, rt.PayloadCID.String(),
+			metrics.CARExport, rt.CARExport.Bool(),
+			metrics.Status, task.Status.Int(),
 			"duration (ms)", duration)
 		return nil
 	}
 	if task.StorageTask.Exists() {
 		st := task.StorageTask.Must()
 		lr.log.Infow("storage task",
-			metrics.UUID, simpleGoValue(&task.UUID),
-			metrics.Miner, simpleGoValue(&st.Miner),
-			metrics.MaxPriceAttoFIL, simpleGoValue(&st.MaxPriceAttoFIL),
-			metrics.Size, simpleGoValue(&st.Size),
-			metrics.StartOffset, simpleGoValue(&st.StartOffset),
-			metrics.FastRetrieval, simpleGoValue(&st.FastRetrieval),
-			metrics.Verified, simpleGoValue(&st.Verified),
-			metrics.Status, simpleGoValue(&task.Status),
+			metrics.UUID, task.UUID.String(),
+			metrics.Miner, st.Miner.String(),
+			metrics.MaxPriceAttoFIL, st.MaxPriceAttoFIL.Int(),
+			metrics.Size, st.Size.Int(),
+			metrics.StartOffset, st.StartOffset.Int(),
+			metrics.FastRetrieval, st.FastRetrieval.Bool(),
+			metrics.Verified, st.Verified.Bool(),
+			metrics.Status, task.Status.Int(),
 			"duration (ms)", duration)
 		return nil
 	}
