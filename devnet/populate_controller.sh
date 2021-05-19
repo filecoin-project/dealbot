47dc92373311e7d956fa46592a011193be6d2300
Will Scott <will.scott@protocol.ai>
2021-05-18 19:41:18 -0700

    fix bugs in ingest script

--- devnet/populate_controller.sh
+++ devnet/populate_controller.sh
@@ -9,17 +9,17 @@ fi
 LEN=$(cat $DATAFILE | jq "length")
 for (( i=0; i<$LEN; i++))
 do
-  DATA=$(cat $DATAFILE | jq ".[$i]")
-  STORAGE=$(cat $DATAFILE | jq ".[$i].size!=null")
+  DATA=$(cat $DATAFILE | jq -c ".[$i]")
+  STORAGE=$(cat $DATAFILE | jq -c ".[$i].size!=null")
   if [ "$STORAGE" = "true" ]; then
     curl --header "Content-Type: application/json" \
       --request POST \
-      --data '$DATA' \
+      --data "$DATA" \
       "$CONTROLLER_ENDPOINT/tasks/storage"
   else
     curl --header "Content-Type: application/json" \
       --request POST \
-      --data '$DATA' \
+      --data "$DATA" \
       "$CONTROLLER_ENDPOINT/tasks/retrieval"
   fi
 done
