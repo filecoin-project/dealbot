49e6881a9c4cc61a9b4f56c3aa5397b2f12065b8
Will Scott <will.scott@protocol.ai>
2021-06-13 18:42:37 -0700

    include payloadCID in finalized task

--- tasks/gen.go
+++ tasks/gen.go
@@ -139,6 +139,7 @@ func main() {
 		schema.SpawnStructField("MinerVersion", "String", true, true),
 		schema.SpawnStructField("ClientVersion", "String", true, true),
 		schema.SpawnStructField("Size", "Int", true, true),
+		schema.SpawnStructField("PayloadCID", "String", true, true),
 	}, schema.SpawnStructRepresentationMap(map[string]string{})))
 	ts.Accumulate(schema.SpawnList("FinishedTasks", "FinishedTask", false))
 	ts.Accumulate(schema.SpawnLinkReference("Link_FinishedTask", "FinishedTask"))
