56933bcfbe1921f335854b4c6ad086d3e2adcc75
ognots <ognots@protonmail.com>
2021-07-20 16:37:08 -0400

    run gofmt on spawner.go

--- controller/spawn/spawner.go
+++ controller/spawn/spawner.go
@@ -14,19 +14,19 @@ var (
 )
 
 type Daemon struct {
-	Id         string   `json:"id,omitempty"`
-	Region     string   `json:"region,omitempty"`
-	Tags       []string `json:"tags,omitempty"`
-	Workers    int      `json:"workers,omitempty"`
-	MinFil     big.Int  `json:"minfil,omitempty"`
-	MinCap     big.Int  `json:"mincap,omitempty"`
-	DockerRepo string   `json:"dockerrepo,omitempty"`
-	DockerTag  string   `json:"dockerrtag,omitempty"`
+	Id               string   `json:"id,omitempty"`
+	Region           string   `json:"region,omitempty"`
+	Tags             []string `json:"tags,omitempty"`
+	Workers          int      `json:"workers,omitempty"`
+	MinFil           big.Int  `json:"minfil,omitempty"`
+	MinCap           big.Int  `json:"mincap,omitempty"`
+	DockerRepo       string   `json:"dockerrepo,omitempty"`
+	DockerTag        string   `json:"dockerrtag,omitempty"`
 	HelmChartVersion string   `json:"helmchartversion,omitempty"`
 	HelmChartRepoUrl string   `json:"helmchartrepourl,omitempty"`
 	LotusDockerRepo  string   `json:"lotusdockerrepo,omitempty"`
-	LotusDockerTag   string   `json:"lotusdockertag,omitempty"`	
-	Wallet     *Wallet  `json:"wallet,omitempty"`
+	LotusDockerTag   string   `json:"lotusdockertag,omitempty"`
+	Wallet           *Wallet  `json:"wallet,omitempty"`
 }
 
 type Wallet struct {
